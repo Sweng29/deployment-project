45fa7cf502b59f9250461512b893e8169bd9b6ec
Kashif Ali <kashifali@ophycare.com>
2024-07-28 13:14:52 +0400

    Deployment demo

--- src/main/java/com/sample/depolyment/deployment_project/UserController.java
+++ src/main/java/com/sample/depolyment/deployment_project/UserController.java
@@ -11,7 +11,21 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
+
+    Map<String, String> users = new HashMap<>();
+
     @GetMapping
+    public Map<String, String> getAllUsers(){
+        buildUsers();
+        return users;
+    }
+
+    private void buildUsers() {
+        users.put("Ali", "Hey!");
+        users.put("Haider", "How are you!");
+    }
+
+    @GetMapping("hello-world")
     public String helloWorld(){
       return "Hello, World!";
     }
